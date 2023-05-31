0d0b3bd9695651783d5cd8d83bede127c972cb94
Hart102 <hartongit102@gmail.com>
2023-05-30 20:08:18 -0400

    worked on the lend book route

--- server/Controller/BooksController/booksController.js
+++ server/Controller/BooksController/booksController.js
@@ -1,27 +1,49 @@
+const { ObjectId } = require("mongodb");
 const { db } = require("../NewDataBase/DatabaseConnection");
+
+
+const check_borrowing_limit = (regNo) => { // check borrowing limit of member
+    // return console.log(regNo)
+    return db.collection('members').findOne({ RegNo: regNo }).then(result => {
+        if(result) return { status: true, success: result }
+        return { status: false }
+    })
+}
 
 const lend_book_function = () => {
     return{
         
-        check_borrowing_limit (){ // check borrowing limit of member
-            db.collection('members').findOne({RegNo: req.body.RegNo}).then(result => 
-            {
-                if(result){
-                    return result
-                    if (Number(result.borrowing_limit) < 4) return ({success: true, data: result})
-                    return ({error: false})
+        // check_borrowing_limit (){ // check borrowing limit of member
+        //     db.collection('members').findOne({RegNo: req.body.RegNo}).then(result => 
+        //     {
+        //         if(result){
+        //             return result
+        //             if (Number(result.borrowing_limit) < 4) return ({success: true, data: result})
+        //             return ({error: false})
 
-                }else{
-                    return({error: "Member not found"})
-                }
-            })
-        },
+        //         }else{
+        //             return({error: "Member not found"})
+        //         }
+        //     })
+        // },
 
-        lend_books (req, res){
-            console.log(lend_book_function().check_borrowing_limit)
+     
+
+        lend_books (req, res){ // userId: 646809c89002c9f6314576b0
+            // return console.log(req.body.RegNo)
+
+            const status = check_borrowing_limit(req.body.RegNo)
+            console.log(status)
+
+            // db.collection('members').findOne({ RegNo: req.body.RegNo }).then(result => {
+            //     if(result) return console.log({ status: true, success: result })
+            //     return console.log({ status: false })
+            // })
         }
         
 
+
+        
 
 
 
